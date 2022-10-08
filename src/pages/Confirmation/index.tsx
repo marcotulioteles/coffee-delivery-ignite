4adac374226ce3744d2c23623a83af2354014465
Marco Túlio Teles <marcotuliocivileng@gmail.com>
2022-10-08 16:32:21 -0300

    feat: finish success page layout

--- src/pages/Confirmation/index.tsx
+++ src/pages/Confirmation/index.tsx
@@ -1,11 +1,47 @@
-import { ConfirmationContainer, ConfirmationContent, PurchaseDetailsContainer } from "./styles";
+import deliveryGuy from '../../assets/delivery-guy.svg';
+import { CurrencyDollar, MapPin, Timer } from "phosphor-react";
+import { defaultTheme } from "../../styles/themes/default";
+import { ConfirmationContainer, ConfirmationContent, DetailsCircleIcon, PurchaseDetailsContainer, PurchaseDetailsText, PurchaseDetailsTextGroup, TitleWrapper } from "./styles";
 
 export function Confirmation() {
   return (
     <ConfirmationContainer>
       <ConfirmationContent>
-        <PurchaseDetailsContainer></PurchaseDetailsContainer>
+        <TitleWrapper>
+          <h1>Uhu! Pedido confirmado</h1>
+          <p>Agora é só aguardar que logo o café chegará até você</p>
+        </TitleWrapper>
+        <PurchaseDetailsContainer>
+          <PurchaseDetailsTextGroup>
+            <DetailsCircleIcon backgroundColor={ defaultTheme.purple }>
+              <MapPin size={ 16 } color="#FFFFFF" weight="fill"/>
+            </DetailsCircleIcon>
+            <PurchaseDetailsText>
+              <p>Entrega em <span>Rua João Daniel Martinelli, 102</span></p>
+              <p>Farrapos - Porto Alegre, RS</p>
+            </PurchaseDetailsText>
+          </PurchaseDetailsTextGroup>
+          <PurchaseDetailsTextGroup>
+            <DetailsCircleIcon backgroundColor={ defaultTheme.yellow }>
+              <Timer size={ 16 } color="#FFFFFF" weight="fill"/>
+            </DetailsCircleIcon>
+            <PurchaseDetailsText>
+              <p>Previsão de entrega</p>
+              <span>20 min - 30 min</span>
+            </PurchaseDetailsText>
+          </PurchaseDetailsTextGroup>
+          <PurchaseDetailsTextGroup>
+            <DetailsCircleIcon backgroundColor={ defaultTheme["yellow-dark"] }>
+              <CurrencyDollar size={ 16 } color="#FFFFFF" weight="fill"/>
+            </DetailsCircleIcon>
+            <PurchaseDetailsText>
+              <p>Pagamento na entrega</p>
+              <span>Cartão de Crédito</span>
+            </PurchaseDetailsText>
+          </PurchaseDetailsTextGroup>
+        </PurchaseDetailsContainer>
       </ConfirmationContent>
+      <img src={ deliveryGuy }/>
     </ConfirmationContainer>
   )
 }
--- src/pages/Confirmation/styles.ts
+++ src/pages/Confirmation/styles.ts
@@ -1,16 +1,21 @@
 import styled from 'styled-components';
 
+interface DetailsCircleIconProps {
+  backgroundColor: string;
+}
+
 export const ConfirmationContainer = styled.div`
   display: flex;
-  align-items: center;
+  align-items: flex-end;
+  justify-content: space-between;
   width: 1180px;
-  margin: 0 auto;
+  margin: 80px auto 0;
 `;
 
 export const ConfirmationContent = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  gap: 40px;
   width: 50%;
 `;
 
@@ -36,9 +41,43 @@ export const TitleWrapper = styled.div`
 `;
 
 export const PurchaseDetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
   padding: 40px;
   background: linear-gradient(${ props => props.theme['base-background'] }, ${ props => props.theme['base-background'] }) padding-box,
   linear-gradient(to right, ${ props => props.theme.yellow }, ${ props => props.theme.purple }) border-box;
   border-radius: 6px 36px 6px 36px;
   border: 1px solid transparent;
+`;
+
+export const PurchaseDetailsTextGroup = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const DetailsCircleIcon = styled.div<DetailsCircleIconProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  background-color: ${ props => props.backgroundColor };
+`;
+
+export const PurchaseDetailsText = styled.div`
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+  line-height: 1.3;
+  color: ${ props => props.theme['base-text'] };
+
+  p {
+    font-weight: 400;
+  }
+
+  span {
+    font-weight: 700;
+  }
 `;
